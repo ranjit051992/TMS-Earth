33f7e906c095c13cb8640c910b490ed4dbe81a60
Om Pawar <om.pawar@zycus.com>
2020-02-19 20:57:51 +0530

    database testdata and uielements implementation

--- automation/Framework/FrameworkUtilities/DatabaseOperations/databaseOperations.js
+++ automation/Framework/FrameworkUtilities/DatabaseOperations/databaseOperations.js
@@ -40,14 +40,95 @@ module.exports = {
                                 logger.info("Inside rows for loop");
 
                                 for (let [key, value] of Object.entries(rows[i])) {
-                                    if(counter === 0) {
+                                    if(key === "FIELD_NAME") {
                                         mapKey = value;
                                     }
-                                    else {
+                                    else if(key === columnName) {
                                         mapValue = value;
                                     }
-                                    counter++;
+
+
+                                    // if(counter === 0) {
+                                    //     mapKey = value;
+                                    // }
+                                    // else {
+                                    //     mapValue = value;
+                                    // }
+                                    // counter++;
                                 }
+                                testDataMap.set(mapKey, mapValue);
+                            }
+                            connection.destroy();
+                            logger.info(`Data map size --> ${testDataMap.size}`);
+                            resolve(testDataMap);
+                        }
+                    });
+                }
+            });
+        });
+    },
+
+    async getUiElementXpath(uiElementKey) {
+
+        const query = `SELECT PAGE_NAME, ELEMENT_NAME, XPATH FROM eProc_UI_Elements`;
+
+        return new Promise((resolve, reject) => {
+            let testDataMap = new Map();
+
+            logger.info("Creating sql connection");
+            connection = mysql.createConnection({
+                host: "192.168.5.221",
+                user: "gauge.poc",
+                password: "gauge@123",
+                database: "Gauge_schema",
+            });
+
+            logger.info("Checking sql connection");
+            connection.connect(function (error) {
+                if (!!error) {
+                    logger.info("Error");
+                }
+                else {
+                    logger.info("Connected");
+                    logger.info("Triggering sql query");
+                    connection.query(query, function (error, rows, fields) {
+                        if (!!error) {
+                            logger.info("Error in the query");
+                        }
+                        else {
+                            logger.info("SUCCESS!");
+
+                            for(let i = 0; i < rows.length; i++) {
+                                let mapKey;
+                                let mapValue;
+
+                                for (let [key, value] of Object.entries(rows[i])) {
+                                    if(key === "PAGE_NAME") {
+                                        mapKey = value;
+                                    }
+                                    else if(key === "ELEMENT_NAME") {
+                                        mapKey = `${mapKey}/${value}`;
+                                    }
+                                    else if(key === "XPATH") {
+                                        mapValue = value;
+                                    }
+
+                                }
+                                
+                                // if(mapKey === uiElementKey) {
+                                //     connection.destroy();
+                                //     logger.info(`Ui element found ${mapValue}`);
+                                //     resolve(mapValue);
+                                //     break;
+                                // }
+                                // else {
+                                //     if(i === (rows.length - 1)) {
+                                //         connection.destroy();
+                                //         logger.info(`Ui element key ${uiElementKey} not present in table`);
+                                //         reject(`Ui element key ${uiElementKey} not present in table`);
+                                //     }
+                                // }
+                                
                                 testDataMap.set(mapKey, mapValue);
                             }
                             connection.destroy();
--- automation/eProc/implementation/Requisition/OnlineStore/OnlineStoreImpl.js
+++ automation/eProc/implementation/Requisition/OnlineStore/OnlineStoreImpl.js
@@ -1,9 +1,8 @@
 const { I } = inject();
 const logger = require("../../../../Framework/FrameworkUtilities/Logger/logger");
-const dataMap = require("../../../../Framework/FrameworkUtilities/DatabaseOperations/databaseOperations");
+const databaseOperations = require("../../../../Framework/FrameworkUtilities/DatabaseOperations/databaseOperations");
 const dbKeys = require("../../../../Framework/FrameworkUtilities/DatabaseOperations/dbKeys");
 const iOnlineStore = require("./IOnlineStore");
-
 const username = "auto.zcs1@zycus.com";
 const password = "eProc@123";
 
@@ -11,52 +10,64 @@ let map = new Map();
 
 Given('logged in on eproc Page', async () => {
     I.amOnPage("https://login-partner.zycus.com/sso/login");
-    I.seeElement(iOnlineStore.emailAddressGhostTextbox);
+    I.seeElement(global.uiElements.get(iOnlineStore.EMAIL_ADDRESS_GHOST_TEXTBOX));
     logger.info("Navigated to login page");
-    I.click(iOnlineStore.emailAddressGhostTextbox);
-    I.fillField(iOnlineStore.emailAddressTextbox, username);
+    I.click(global.uiElements.get(iOnlineStore.EMAIL_ADDRESS_GHOST_TEXTBOX));
+    I.fillField(global.uiElements.get(iOnlineStore.EMAIL_ADDRESS_TEXTBOX), username);
     logger.info(`Entered email address --> ${username}`);
-    I.click(iOnlineStore.passwordGhostTextbox);
-    I.fillField(iOnlineStore.passwordTextbox, password);
+    I.click(global.uiElements.get(iOnlineStore.PASSWORD_GHOST_TEXTBOX));
+    I.fillField(global.uiElements.get(iOnlineStore.PASSWORD_TEXTBOX), password);
     logger.info(`Entered password --> ${password}`);
-    I.click(iOnlineStore.loginButton);
+    I.click(global.uiElements.get(iOnlineStore.LOGIN_BUTTON));
     logger.info("Clicked on Login button");
-    I.waitForVisible(iOnlineStore.ddsLoginPage);
+    I.waitForVisible(global.uiElements.get(iOnlineStore.DDS_LOGIN_PAGE));
     I.amOnPage("https://dewdrops-partner.zycus.com/eproc/#/online-store");
-    I.waitForVisible(iOnlineStore.ddsOnlineStorePageReqTableOptionIcon);
+    I.waitForVisible(global.uiElements.get(iOnlineStore.REQ_TABLE_OPTION_ICON));
     logger.info("Navigated to DDS Online Store page");
 });
 
 Given(/^i am on login page "(.*?)"$/, (name) => {
-
     I.assertEqual(1, 1);
     console.log("hey " + name + " i am on login page");
 });
 
 Given(/^i am main page "(.*?)"$/, (name) => {
-
     I.assertEqual('given', 'given');
     console.log("hey " + name + " i am main page");
 });
 
-Given("i am on Online Store page", () => {
-    I.waitForVisible(iOnlineStore.ddsOnlineStorePageReqTableOptionIcon);
+Given("i am on Online Store page", async () => {
+    I.waitForVisible(global.uiElements.get(iOnlineStore.REQ_TABLE_OPTION_ICON));
     logger.info("Navigated to DDS Online Store page");
 });
 
 When("i search catalog item", async () => {
-    map = await dataMap.getTestData();
-    logger.info(`Data map size in onlineStore.js file --> ${map.size}`);
-    const searchItem = map.get(dbKeys.ITEM_NAME_FOR_SEARCHING);
-    I.fillField(iOnlineStore.searchTextbox, searchItem);
+    const searchItem = global.testData.get(dbKeys.ITEM_NAME_FOR_SEARCHING);
+    I.fillField(global.uiElements.get(iOnlineStore.SEARCH_TEXTBOX), searchItem);
     logger.info(`Entered search text --> ${searchItem}`);
     I.pressKey("Enter");
     logger.info("Pressed Enter on search textbox");
 });
 
 Then("results for catalog item are displayed", () => {
-    const searchItem = map.get(dbKeys.ITEM_NAME_FOR_SEARCHING);
+    const searchItem = global.testData.get(dbKeys.ITEM_NAME_FOR_SEARCHING);
     let searchResultXpath = `//eproc-product-list//span[contains(text(),'${searchItem}')]`;
-    I.waitForInvisible(searchResultXpath);
+    I.waitForVisible(searchResultXpath);
     logger.info(`Search results displayed for --> ${searchItem}`);
+});
+
+When("cart is empty", () => {
+    logger.info("cart is empty");
+});
+
+When("add quantity", () => {
+    logger.info("add quantity");
+});
+
+When("click on Add to Cart button", () => {
+    logger.info("click on Add to Cart button");
+});
+
+Then("item should be added to cart", () => {
+    logger.info("item should be added to cart");
 });
