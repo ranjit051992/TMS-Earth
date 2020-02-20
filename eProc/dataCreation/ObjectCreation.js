8ef1e0c7a9fa6430f74bdc2cd59be78818f1a6a0
Om Pawar <om.pawar@zycus.com>
2020-02-20 21:21:17 +0530

    Create spo keyword

--- eProc/dataCreation/ObjectCreation.js
+++ eProc/dataCreation/ObjectCreation.js
@@ -1,29 +1,24 @@
 const spo = require("../bo/Spo");
-
 class ObjectCreation
 {
     getObjectOfStandardPO(noOfItems,itemType)
     {
-        spo.setPoNumber("");
-        spo.setPoDescription("");
-        spo.setPurchaseType("");
-        spo.setSupplierName("");
-        spo.setSupplierAddress("");
-        spo.setPaymentTerm("");
-        spo.setDeliveryTerm("");
-        spo.setCurrency("");
-        spo.setBuyer("");
-        spo.setBookCostAtLineItemLevel("");
-        spo.setBookCostToSingleMultipleCC("");
-        spo.setAssignCostProject("");
-        spo.setItemName("");
-        spo.setGlAccount("");
-        spo.setCostCenter("");
+        //spo.setPoNumber();
+        spo.setPoDescription("AutomationSPO");
+        spo.setPurchaseType(global.testData.get("PURCHASE_TYPE"));
+        spo.setSupplierName(global.testData.get("SUPPLIER_NAME"));
+        spo.setSupplierAddress(global.testData.get("SUPPLIER_ADDRESS"));
+        spo.setPaymentTerm(global.testData.get("PAYMENT_TERMS"));
+        spo.setDeliveryTerm(global.testData.get("DELIVERY_TERMS"));
+        spo.setCurrency(global.testData.get("CURRENCY_TYPE"));
+        spo.setBuyer(global.testData.get("BUYER_NAME"));
+        spo.setBookCostAtLineItemLevel("No");
+        spo.setBookCostToSingleMultipleCC("Yes");
+        spo.setAssignCostProject("No");
+        spo.setItemName(global.testData.get("ITEM_NAME_FOR_SEARCHING"));
+        spo.setGlAccount(global.testData.get("GL_ACCOUNT"));
+        spo.setCostCenter(global.testData.get("COST_CENTER"));
         return spo;
     }
-
 }
-
-getObjectOfStandardPO();
-
 module.exports = new ObjectCreation();
