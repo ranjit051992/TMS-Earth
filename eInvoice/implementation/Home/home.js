b208d8beabf489cdc82e91079ce0c09825fced7a
Awanish Ranjan <awanish.ranjan@zycus.com>
2020-03-05 16:04:19 +0530

    updated code

--- eInvoice/implementation/Home/home.js
+++ eInvoice/implementation/Home/home.js
@@ -7,3 +7,15 @@ Given('I navigate to Invoice module', function () {
 Given('I navigate to CreditMemo module', function () {
     homePage.selectModule('eInvoice','Credit Memos');
 });
+Given('I navigate to setup module', function () {
+    homePage.selectModule('eInvoice','Setup');
+});
+Given('I navigate to TMS', function () {
+    homePage.selectModule('TMS','');
+});
+Given('I navigate to workflow module', function () {
+    homePage.selectModule('eInvoice','Workflow');
+});
+Given('I navigate to workflow module', function () {
+    homePage.selectModule('eInvoice','eForm');
+});
