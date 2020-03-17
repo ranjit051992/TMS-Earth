d12d3933870998cf006ac6c992a0c08ed6df0382
Priyanka Ingale <priyanka.ingale@zycus.com>
2020-03-17 21:57:39 +0530

    added new gherkin Impl

--- eProc/implementation/Requisition/Checkout/CheckoutImpl.js
+++ eProc/implementation/Requisition/Checkout/CheckoutImpl.js
@@ -1179,8 +1179,8 @@ module.exports = {
         
         if(reqBO.convertToPoFlag) {
             await I.wait(prop.DEFAULT_MEDIUM_WAIT);
-           // await I.amOnPage(prop.DDS_BuyersDesk_Url);
-            await commonComponent.navigateToPage(lmtVar.getLabel("APPLICATION_NAME"), lmtVar.getLabel("BUYERS_DESK_LISTING_PAGE"));
+            await I.amOnPage(prop.DDS_BuyersDesk_Url);
+           // await commonComponent.navigateToPage(lmtVar.getLabel("APPLICATION_NAME"), lmtVar.getLabel("BUYERS_DESK_LISTING_PAGE"));
             await I.waitForVisible(I.getElement(poListingObject.PO_NUMBER_LINK));
             await commonComponent.searchDocOnListing(reqNumber, lmtVar.getLabel("SEARCH_BY_DOC_NUMBER"));
             status = await commonComponent.getValueForColumnName(lmtVar.getLabel("STATUS_COLUMN"));
