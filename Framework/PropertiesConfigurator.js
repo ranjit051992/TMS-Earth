4ba39a443d191d2833c357e526d019eb579d0548
Nitin Chourey <nitin.chourey@zycus.com>
2020-03-20 13:27:21 +0000

    PropertiesConfigurator.js edited online with Bitbucket

--- Framework/PropertiesConfigurator.js
+++ Framework/PropertiesConfigurator.js
@@ -1,19 +1,28 @@
 function parseJson() {
-  var PRODUCT =process.env.PRODUCT;
+  var PRODUCT = process.env.PRODUCT;
   var fs = require('fs');
   var data = fs.readFileSync("./Resources/config.json", "utf8");
   var obj = JSON.parse(data);
+  let testDataMap = new Map();
 
   Object.keys(obj).forEach(function (productName) {
-      if (productName === PRODUCT) {
-          console.log("adding config data for :  " + PRODUCT)
-          LMTMap = obj[productName];
-          global.confi_prop=LMTMap;
-      }
+    if (productName === PRODUCT) {
+      console.log("adding config data for :  " + PRODUCT)
+      let LMTMap = obj[productName];
+      global.confi_prop = LMTMap;
+      
+    }
   })
+
+  for(let i=0;i<Object.entries(global.confi_prop).length;i++)
+  {
+   testDataMap.set(Object.entries(global.confi_prop)[i][0],Object.entries(global.confi_prop)[i][1]);
+  }
+  global.configmap=testDataMap; 
+
 }
 
-module.exports =parseJson;
+module.exports = parseJson;
 
 
 
