f65b060ac7ff182ba10a2e280b055aaff07c3643
Nitesh Singh <nitesh.singh@zycus.com>
2020-04-09 20:39:54 +0530

    Added Object repository for Oneview

--- DD_Homes/implementation/OneView/OneViewImpl.js
+++ DD_Homes/implementation/OneView/OneViewImpl.js
@@ -1,15 +1,17 @@
 const { I } = inject();
 const Checkbox = require("dd-cc-zycus-automation/components/checkbox");
 const Filter  = require("dd-cc-zycus-automation/components/filter");
-const CommonKeyword = require("dd-cc-zycus-automation/components/commonKeyword")
+const CommonKeyword = require("dd-cc-zycus-automation/components/commonKeyword");
+const OneViewObject = require("./OneViewObject");
+const DewElement = require("dd-cc-zycus-automation/components/element");
 
 module.exports = {
   /**
      * Navigate to One View
      */
   async navigateOneView() {
-    I.seeElement(`//span[contains(@class,'nav-link')]//div[text()[normalize-space()='OneView']]`)
-    CommonKeyword.clickElement(`//span[contains(@class,'nav-link')]//div[text()[normalize-space()='OneView']]`)
+    I.seeElement(OneViewObject.oneViewLink)
+    CommonKeyword.clickElement(OneViewObject.oneViewLink)
     I.wait(7)
   },
 
@@ -18,48 +20,50 @@ module.exports = {
    * @param {*} supplierName 
    */
   async searchAndSelectSupplier(supplier) {
-    I.seeElement(`//input[@placeholder='Search and select suppliers']`)
-    I.clearField(`//input[@placeholder='Search and select suppliers']`)
-    I.fillField(`//input[@placeholder='Search and select suppliers']`, supplier)
-    I.waitForVisible(`//div[contains(@class,'custom-control custom-checkbox ')]//label/span[text()='${supplier}']`, 10)
-    CommonKeyword.clickElement(`//div[contains(@class,'custom-control custom-checkbox ')]//label/span[text()='${supplier}']`)
-    CommonKeyword.clickElement(`//div[contains(@class,'flex-column')]//div[text()='Done']`)
+    I.seeElement(OneViewObject.oneViewSearchBox)
+    I.clearField(OneViewObject.oneViewSearchBox)
+    I.fillField(OneViewObject.oneViewSearchBox, supplier)
+    const supplierCheckBox = await(DewElement.getDynamicLocator(OneViewObject.oneViewSupplierCheckBox,"<<supplier>>",supplier))
+    I.waitForVisible(supplierCheckBox, 10)
+    CommonKeyword.clickElement(supplierCheckBox)
+    CommonKeyword.clickElement(OneViewObject.oneViewSearchDonebutton)
     I.waitToProcess()
   },
 
 
   async searchAndSelectSuppliers(...suppliers) {
-    I.seeElement(`//input[@placeholder='Search and select suppliers']`)
+    I.seeElement(OneViewObject.oneViewSearchBox)
     suppliers.forEach(async (supplier) => {
-      I.clearField(`//input[@placeholder='Search and select suppliers']`)
-      I.fillField(`//input[@placeholder='Search and select suppliers']`, supplier)
-      I.waitForVisible(`//div[contains(@class,'custom-control custom-checkbox ')]//label/span[[text()='${supplier}']`, 10)
-      CommonKeyword.clickElement(`//div[contains(@class,'custom-control custom-checkbox ')]//label/span[[text()='${supplier}']`)
+      I.clearField(OneViewObject.oneViewSearchBox)
+      I.fillField(OneViewObject.oneViewSearchBox, supplier)
+      const supplierCheckBox = await(DewElement.getDynamicLocator(OneViewObject.oneViewSupplierCheckBox,"<<supplier>>",supplier))
+          I.waitForVisible(supplierCheckBox, 10)
+      CommonKeyword.clickElement(supplierCheckBox)
     });
-    CommonKeyword.clickElement(`//div[contains(@class,'flex-column')]//div[text()='Done']`)
+    CommonKeyword.clickElement(OneViewObject.oneViewSearchDonebutton)
     I.waitToProcess()
   },
 
   async verifySupplierCard(supplier) {
     I.waitToProcess()
-    CommonKeyword.clickElement(`//dew-default-tab-head[text()='Supplier Information ']`)
-    I.waitForElement(`//div[@class='tab-links-wrapper main dew-container']//dew-default-tab-head[text()[normalize-space()='${supplier}']]`,10)
-    I.seeElement(`//div[@class='tab-links-wrapper main dew-container']//dew-default-tab-head[text()[normalize-space()='${supplier}']]`)
+    CommonKeyword.clickElement(OneViewObject.oneViewSupplierInfoTab)
+    const supplierBasicDetails = await(DewElement.getDynamicLocator(OneViewObject.oneViewSupplierBasicDetails,"<<supplier>>",supplier))
+    I.waitForElement(supplierBasicDetails,10)
+    I.seeElement(supplierBasicDetails)
   },
 
   async verifySupplierCards(...suppliers) {
     suppliers.forEach(async (supplier) => {
-      I.seeElement(`//div[@class='tab-links-wrapper main dew-container']//dew-default-tab-head[text()[normalize-space()='${supplier}']]`)
+      I.seeElement(supplierBasicDetails)
     });
   },
 
   async exportFiles() {
   
-    I.scrollIntoView(`//span[contains(@class,'nav-link')]//div[text()[normalize-space()='OneView']]`)
+    I.scrollIntoView(OneViewObject.oneViewLink)
     I.wait(3);
     
-    I.click(`//span[contains(@class,'import')]/..`);
-
+    I.click(OneViewObject.oneViewExportLink);
   },
 
   async filterSupplier(suppliername)
---
+++ DD_Homes/implementation/OneView/OneViewObject.js
@@ -0,0 +1,10 @@
+module.exports= {
+
+    oneViewLink: "//span[contains(@class,'nav-link')]//div[text()[normalize-space()='OneView']]",
+    oneViewSearchBox: "//input[@placeholder='Search and select suppliers']",
+    oneViewSupplierCheckBox: "//div[contains(@class,'custom-control custom-checkbox')]//label/span[text()='<<supplier>>']",
+    oneViewSearchDonebutton: "//div[contains(@class,'flex-column')]//div[text()='Done']",
+    oneViewSupplierInfoTab: "//dew-default-tab-head[text()='Supplier Information ']",
+    oneViewSupplierBasicDetails: "//div[@class='tab-links-wrapper main dew-container']//dew-default-tab-head[text()[normalize-space()='<<supplier>>']]",
+    oneViewExportLink: "//span[contains(@class,'import')]/.."    
+}
